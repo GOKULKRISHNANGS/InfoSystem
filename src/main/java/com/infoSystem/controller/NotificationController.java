bd805727104ff26d8d00453dd2007812fc5647e3
gokul <krishnanasadasivam96@gmail.com>
2018-01-19 21:12:31 +0530

    Created API for notifications

--- src/main/java/com/infoSystem/controller/NotificationController.java
+++ src/main/java/com/infoSystem/controller/NotificationController.java
@@ -1,11 +1,15 @@
 package com.infoSystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,12 +32,13 @@ public class NotificationController {
 		mav.setViewName("notificationsPage");
 		return mav;
 	}
-	
-//	@GetMapping("notifications_page/{registrationNumber}")
-//	public ModelAndView redirect(){
-//		
-//	}
-//	
+
+	@ResponseBody
+	@RequestMapping(value = "notifications", method = RequestMethod.GET)
+	public List<NotificationModel> getNotification() {
+		List<NotificationModel> notificationList = notificationService.getAllNotifications();
+		return notificationList;
+	}
 
 	@GetMapping("new_notification")
 	public ModelAndView createNotification() {
